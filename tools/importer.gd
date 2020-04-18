7a8e23a3dc007f09e80c8200db8c1536f81c53e0
Dan Reeves <hey@danreev.es>
2020-04-18 03:33:20 +0100

    add importer

---
+++ tools/importer.gd
@@ -0,0 +1,24 @@
+tool # Needed so it runs in editor
+extends EditorScenePostImport
+
+# This sample changes all node names
+
+# Called right after the scene is imported and gets the root node
+func postimport(scene):
+	# Change all node names to "modified[oldnodename]"
+	iterate(scene)
+	return scene # Remember to return the imported scene
+
+func iterate(node: Node):
+	if node != null:
+
+		if node is MeshInstance && "-col" in node.get_parent().name:
+			print(node.name + " " + node.get_parent().name)
+			var collider = node.create_trimesh_collision()
+			print(collider)
+			if collider != null:
+				node.add_child(collider)
+
+		for child in node.get_children():
+			if child != null:
+				iterate(child)
